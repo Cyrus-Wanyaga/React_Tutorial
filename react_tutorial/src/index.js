a1366106f0d75d4b6a972873c3cd4ff51b670773
Cyrus-Wanyaga <cwanyaga95@gmail.com>
2019-09-22 14:34:07 +0300

    component lifecycle

--- react_tutorial/src/index.js
+++ react_tutorial/src/index.js
@@ -7,7 +7,7 @@ import { Home } from "./components/Home";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { homeLink: "Home" };
+    this.state = { homeLink: "Home", homeMounted: true };
   }
   onGreet() {
     alert("Hello");
@@ -17,10 +17,15 @@ class App extends React.Component {
       homeLink: newName
     });
   }
+  onChangeHomeMounted() {
+    this.setState({
+      homeMounted: !this.state.homeMounted
+    });
+  }
   render() {
-    return (
-      <div className="container">
-        <Header homeLink={this.state.homeLink} />
+    let homeCmp = "";
+    if (this.state.homeMounted) {
+      homeCmp = (
         <Home
           name={"Max"}
           initialAge={27}
@@ -28,6 +33,20 @@ class App extends React.Component {
           changeLink={this.onChangeLinkName.bind(this)}
           initialLinkName={this.state.homeLink}
         />
+      );
+    }
+    return (
+      <div className="container">
+        <Header homeLink={this.state.homeLink} />
+        <div className="row">{homeCmp}</div>
+        <div className="row">
+          <button
+            className="btn btn-dark btn-sm"
+            onClick={this.onChangeHomeMounted.bind(this)}
+          >
+            (Un)Mount Home Component
+          </button>
+        </div>
         <p>This is a paragraph</p>
       </div>
     );
