173b224c188f736a88813fd9c4f253379c9612d3
Cyrus-Wanyaga <cwanyaga95@gmail.com>
2019-09-23 08:52:26 +0300

    react-router-dom

---
+++ react_tutorial/src/components/Root.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import {Header} from "./Header";
+
+export class Root extends React.Component {
+    render(){return(<div></div>);}
+}
--- react_tutorial/src/index.js
+++ react_tutorial/src/index.js
@@ -5,52 +5,12 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { homeLink: "Home", homeMounted: true };
-  }
-  onGreet() {
-    alert("Hello");
-  }
-  onChangeLinkName(newName) {
-    this.setState({
-      homeLink: newName
-    });
-  }
-  onChangeHomeMounted() {
-    this.setState({
-      homeMounted: !this.state.homeMounted
-    });
-  }
-  render() {
-    let homeCmp = "";
-    if (this.state.homeMounted) {
-      homeCmp = (
-        <Home
-          name={"Max"}
-          initialAge={27}
-          greet={this.onGreet}
-          changeLink={this.onChangeLinkName.bind(this)}
-          initialLinkName={this.state.homeLink}
-        />
-      );
-    }
-    return (
-      <div className="container">
-        <Header homeLink={this.state.homeLink} />
-        <div className="row">{homeCmp}</div>
-        <div className="row">
-          <button
-            className="btn btn-dark btn-sm"
-            onClick={this.onChangeHomeMounted.bind(this)}
-          >
-            (Un)Mount Home Component
-          </button>
-        </div>
-        <p>This is a paragraph</p>
-      </div>
-    );
-  }
-}
+ render(){
+   return(
+     <Root>
+       <Home></Home>
+     </Root>
+   );
+ }
 
 render(<App />, window.document.getElementById("root"));
